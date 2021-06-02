6dce4c0dee4490a4d8b4cf73dc1d2a756777fbd8
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2021-06-02 15:13:14 +0100

    Update UndirectedGraph.java

--- app/UndirectedGraph.java
+++ app/UndirectedGraph.java
@@ -1,29 +1,44 @@
 package app;
 
-public class UndirectedGraph extends Graph{
+public class UndirectedGraph extends Graph {
+    @Override
+    public void addNeighbor(int vIndex, int neighborIndex) {
 
-	@Override
-	public void addNeighbor(int vIndex, int neighborIndex) {
-		// TODO Auto-generated method stub
-		
-	}
+        // v1 neighbor of v2
+        super.getadj()[vIndex].add(neighborIndex);
 
-	@Override
-	public void addNeighbor(String v, String neighbor) {
-		// TODO Auto-generated method stub
-		
-	}
+        //v2 neighbhor of v1 (because non-orienté)
+        super.getadj()[neighborIndex].add(vIndex);
+    }
 
-	@Override
-	public void removeNeighbor(String v, String neighbor) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void addNeighbor(String v, String neighbor) {
+        int vIndex = super.getIndex(v);
+        int neighborIndex= super.getIndex(neighbor);
 
-	@Override
-	public void removeNeighbor(int vIndex, int neighborIndex) {
-		// TODO Auto-generated method stub
-		
-	}
+        addNeighbor(vIndex, neighborIndex);
+        //super.getadj()[vIndex].add(neighborIndex);
+        //super.getadj()[neighborIndex].add(vIndex);
+    }
+
+
+
+    @Override
+    public void removeNeighbor(int vIndex, int neighborIndex){
+
+        super.getadj()[vIndex].remove(neighborIndex);
+        super.getadj()[neighborIndex].remove(vIndex);
+
+    }
+
+    @Override
+    public void removeNeighbor(String v, String neighbor) {
+        int vIndex = super.getIndex(v);
+        int neighborIndex= super.getIndex(neighbor);
+
+        removeNeighbor(vIndex,neighborIndex);
+        //super.getadj()[vIndex].remove(neighborIndex);
+        //super.getadj()[neighborIndex].remove(vIndex);
+    }
 
 }
