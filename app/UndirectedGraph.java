0d9b088cab634843555074ed85a9b20f25d00adb
Djalil <djaloulehez3@gmail.com>
2021-06-06 16:05:50 +0100

    Revert "Update UndirectedGraph.java"
    This reverts commit 9b80f1cb9c5a3e3064da117f3bb537c0611ce6a4.

--- app/UndirectedGraph.java
+++ app/UndirectedGraph.java
@@ -1,33 +1,41 @@
 package app;
 
-    import java.util.ArrayList ;
+public class UndirectedGraph extends Graph {
+    @Override
+    public void addNeighbor(int vIndex, int neighborIndex) {
 
-    public  class DirectedGraph extends Graph {
+        // v1 neighbor of v2
+        super.getAdj()[vIndex].add(neighborIndex);
 
-        @Override
-        public void addNeighbor(int vIndex, int neighborIndex) {
-
-            getAdj()[vIndex].add(neighborIndex)  ;
-
-        }
-
-        @Override
-        public void addNeighbor(String v, String neighbor){
-            int vIndex = getIndex(v);
-            int IndexNeigh = getIndex(neighbor);
-            addNeighbor(vIndex,IndexNeigh);
-        }
-
-        @Override
-        public void removeNeighbor(int vIndex, int neighborIndex) {
-            
-        }
-
-        @Override
-        public void removeNeighbor(String v, String neighbor){
-            int vIndex=super.getIndex(v) ;
-            int IndexNeigh=super.getIndex(neighbor);
-            getAdj()[vIndex].remove(IndexNeigh);
-        }
+        //v2 neighbhor of v1 (because non-orienté)
+        super.getAdj()[neighborIndex].add(vIndex);
     }
 
+    @Override
+    public void addNeighbor(String v, String neighbor) {
+        int vIndex = super.getIndex(v);
+        int neighborIndex= super.getIndex(neighbor);
+
+        if((vIndex != -1) && (neighborIndex != -1))
+        addNeighbor(vIndex, neighborIndex);
+    }
+
+
+
+    @Override
+    public void removeNeighbor(int vIndex, int neighborIndex){
+        super.getAdj()[vIndex].remove(neighborIndex);
+        super.getAdj()[neighborIndex].remove(vIndex);
+
+    }
+
+    @Override
+    public void removeNeighbor(String v, String neighbor) {
+        int vIndex = super.getIndex(v);
+        int neighborIndex= super.getIndex(neighbor);
+
+        if((vIndex != -1) && (neighborIndex != -1))
+        removeNeighbor(vIndex,neighborIndex);
+    }
+
+}
